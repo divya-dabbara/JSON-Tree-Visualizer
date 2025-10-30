03211fc2faf13be2f7e9cb27a30c03165b774f3d
Divya Dabbara <divyadabbara76@gmail.com>
2025-10-30 21:26:26 +0530

    fix: corrected base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),React()],
-  base: '/json-tree-visualizer/'
+  base: './',
 })
